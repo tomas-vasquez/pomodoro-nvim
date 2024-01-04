2bb19936dfe50fb19d72171fee7d99b84b4c7576
Tomas Vasquez <tomasdetloging@gmail.com>
2024-01-04 06:17:44 -0400

    fix command

--- lua/init.lua
+++ lua/init.lua
@@ -4,6 +4,6 @@ local pomodoro = require("pomodoro")
 pomodoro.setup()
 
 -- Definir el comando personalizado para iniciar el Pomodoro
-vim.cmd([[command! PomodoroStart lua require('pomodoro').start()]])
+vim.cmd([[command! PomodoroStart lua require('pomodoro').start_pomodoro()]])
 
 -- Resto de tu configuración
